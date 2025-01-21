303dfa7b3591502720869f1fdf0ce84589bd57bc
harshsingh-byteIQ <harshsingh.byteiq@gmail.com>
2025-01-22 00:13:24 +0530

    final scroll fixed

--- src/Components/Trending/index.tsx
+++ src/Components/Trending/index.tsx
@@ -367,19 +367,36 @@ const Trending = () => {
   let lastVal = 0;
   const handleScroll = (event: React.UIEvent<HTMLDivElement>) => {
     const container = event.currentTarget;
-    const scrollPosition = container.scrollTop;
-    if (Math.abs(scrollPosition - lastVal) < 20) return;
     const videoHeight = container.scrollHeight / 2;
-    const nearestVideoIndex = Math.round(scrollPosition / videoHeight);
+    const scrollPosition = container.scrollTop;
+    
+    if(Math.abs(scrollPosition - lastVal) < 100) {
+      return;
+    }
+
+    const isScrollingDown = scrollPosition > lastVal;
+    lastVal = scrollPosition; 
+
+    const currentIndex = Math.round(scrollPosition / videoHeight);
+    let targetIndex = currentIndex;
+  
+    if (isScrollingDown) {
+      targetIndex = Math.min(currentIndex + 1, Math.floor(container.scrollHeight / videoHeight) - 1); // Limit to last reel
+    } else {
+      targetIndex = Math.max(currentIndex - 1, 0);
+    }
+  
+   
     container.scrollTo({
-      top: nearestVideoIndex * videoHeight,
+      top: targetIndex * videoHeight,
       behavior: "smooth",
     });
-    lastVal = scrollPosition;
+  
     if (trackState) {
       setTrackState(false);
     }
   };
+  
 
   return (
     <div className={styles.sidebar}>
